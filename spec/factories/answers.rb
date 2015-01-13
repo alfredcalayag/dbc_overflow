417e9303b124b028615ba44a676dd9210f556f70
alfredcalayag <rapadura05@gmail.com>
2015-01-13 11:05:19 -0800

    Create factories for questions and answers

---
+++ spec/factories/answers.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :answer do
+    content "My content"
+    question_id 1
+    user_id 1
+  end
+end
---
+++ spec/factories/questions.rb
@@ -0,0 +1,8 @@
+
+FactoryGirl.define do
+  factory :question do
+    title "My Title"
+    content "My content"
+    user_id 1
+  end
+end
