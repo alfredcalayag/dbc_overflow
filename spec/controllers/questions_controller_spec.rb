486adf88766a98dd4fb924e0a1e54b13cab3b620
alfredcalayag <rapadura05@gmail.com>
2015-01-13 11:59:33 -0800

    Create questions_controller_spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe QuestionsController, :type => :controller do
+describe QuestionsController do
+# Test the index and show page
+  describe "GET #index" do
+    it "renders the :index template"
+  end
+
+  describe "GET #show" do
+    it "renders the :show template"
+  end
 
 end
