a6e43176ac491e224531de05172fd53e91677a1f
alfredcalayag <rapadura05@gmail.com>
2015-01-13 10:58:44 -0800

    Minor revision to question_spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Question, :type => :model do
-  context "Validation" do
+  context "Validations" do
 
     it "has a valid factory" do
       expect( build(:question) ).to be_valid
@@ -14,22 +14,21 @@ describe Question, :type => :model do
 
     it "is invalid without a title" do
       question = build(:question, title: nil)
-      p question.valid?
-      # p question.errors
+      question.valid?
       expect(question.errors[:title]).to include("can't be blank")
     end
 
+    it "is invalid without content" do
+      question = build(:question, content: nil)
+      question.valid?
+      expect(question.errors[:content]).to include("can't be blank")
+    end
 
-
-  end
-
-  context "Does not validate unless all attributes are present" do
-    it "Does not save if title is blank"
-    it "Returns an error if the title is blank"
-    it "Does not save if content is blank"
-    it "Returns an error if the content is blank"
-    it "Does not save without an associated user_id"
-    it "Returns an error if there is no associated user_id"
+    it "is invalid without an associated user_id" do
+      question = build(:question, user_id: nil)
+      question.valid?
+      expect(question.errors[:user_id]).to include("can't be blank")
+    end
   end
 
 end
