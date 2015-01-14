4981e6a699c95e7fad8bdff4e15fa81031bd9e00
alfredcalayag <rapadura05@gmail.com>
2015-01-13 21:22:47 -0800

    Add capybara feature tests.

--- spec/features/questions_controller_spec.rb
+++ spec/features/questions_controller_spec.rb
@@ -7,16 +7,24 @@
 
 require 'rails_helper'
 
-feature 'questions#index' do
-  scenario "adds a new question" do
+
+describe "questions/index.html.erb" do
+  let!(:question) { create :question }
+  before (:each) do
+    visit root_path
   end
+
+  it "displays a list of all questions" do
+    expect(page).to have_content(question.title)
+  end
+
+  it "can create a new question" do
+    question = FactoryGirl.attributes_for :question
+    fill_in "title", :with => question[:title]
+    fill_in "content", :with => question[:content]
+    fill_in "user_id", :with => question[:user_id]
+    click_on "Create"
+    expect(page).to have_content question[:title]
+  end
+
 end
-
-# feature 'questions#show' do
-#   scenario "adds a new answer"
-#   scenario "has a link to delete the question"
-# end
-
-# feature 'questions#edit' do
-
-# end
