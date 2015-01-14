37304e3faf0be479d6ec24951cdd4490a0eed479
alfredcalayag <rapadura05@gmail.com>
2015-01-14 10:20:48 -0800

    Question show page displays answers and can add new answers

---
+++ spec/features/questions.show.html.erb_spec.rb
@@ -0,0 +1,34 @@
+# Questions Controller Spec
+  # Index page has a new question form
+  # Show page has a new answer form
+  # Show page has a destroy question link
+  # Edit page exists
+  # Edit page has an update question form
+
+require 'rails_helper'
+
+describe "questions/show.html.erb", :js => true do
+  let!(:question) { create(:question) }
+  let!(:answer) { create(:answer, question_id: question.id) }
+  before (:each) do
+    # create(:answer, question_id: question.id)
+    visit question_path(question)
+  end
+
+  it "displays the question" do
+    expect(page).to have_content(question.content)
+  end
+
+  it "displays all previous answers to the question" do
+    expect(page).to have_content(answer.content)
+  end
+
+  it "can create a new answer" do
+    answer = FactoryGirl.attributes_for :answer
+    fill_in "content", :with => answer[:content]
+    click_on "Submit Answer"
+
+    expect(page).to have_content answer[:content]
+  end
+
+end
