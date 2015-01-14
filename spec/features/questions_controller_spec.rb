986d5da0de529513d2b3f3767ba47ad2df93772f
alfredcalayag <rapadura05@gmail.com>
2015-01-13 20:43:02 -0800

    Add features spec for questions controller

--- spec/features/questions_controller_spec.rb
+++ spec/features/questions_controller_spec.rb
@@ -4,3 +4,19 @@
   # Show page has a destroy question link
   # Edit page exists
   # Edit page has an update question form
+
+require 'rails_helper'
+
+feature 'questions#index' do
+  scenario "adds a new question" do
+  end
+end
+
+# feature 'questions#show' do
+#   scenario "adds a new answer"
+#   scenario "has a link to delete the question"
+# end
+
+# feature 'questions#edit' do
+
+# end
