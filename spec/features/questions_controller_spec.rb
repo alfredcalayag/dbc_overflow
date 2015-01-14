ffcac54ca3ccd091b51637cf8a0fe118abe74b99
alfredcalayag <rapadura05@gmail.com>
2015-01-13 19:02:29 -0800

    Create features spec for questions_controller

---
+++ spec/features/questions_controller_spec.rb
@@ -0,0 +1,6 @@
+# Questions Controller Spec
+  # Index page has a new question form
+  # Show page has a new answer form
+  # Show page has a destroy question link
+  # Edit page exists
+  # Edit page has an update question form
