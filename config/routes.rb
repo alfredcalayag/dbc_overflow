b658dd67fe1d03249b1f8831f3718f16b3e8573a
alfredcalayag <rapadura05@gmail.com>
2015-01-13 18:37:16 -0800

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'questions/index'
+  resources :questions
+  root 'questions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -13,7 +14,6 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  get 'questions/:id' => 'questions#show'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
