70cb66489721d854712597cabd388f68f60c7360
alfredcalayag <rapadura05@gmail.com>
2015-01-13 18:29:56 -0800

    Add controller specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  get 'questions/:id' => 'questions#show'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,12 +2,44 @@ require 'rails_helper'
 
 describe QuestionsController do
 # Test the index and show page
-  describe "GET #index" do
-    it "renders the :index template"
-  end
+
+    describe "#index lists all questions" do
+      it "assigns @questions to Question.all" do
+        question1 = create(:question, title: "Q1")
+        question2 = create(:question, title: "Q2")
+        get :index
+        expect(assigns(:questions)).to eq Question.all
+      end
+
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
 
   describe "GET #show" do
-    it "renders the :show template"
+    it "assigns the requested question to @question" do
+      question = create(:question)
+      get :show, id: question
+      expect( assigns(:question) ).to eq question
+    end
+
+    it "assigns all associated answers to @answers" do
+      question = create(:question)
+      answer1 = create(:answer, question_id: question.id)
+      answer2 = create(:answer, question_id: question.id)
+      answer3 = create(:answer, question_id: 99999999)
+      get :show, id: question
+      expect(assigns(:answers)).to eq([answer1, answer2])
+      expect(assigns(:answers)).to_not eq(Answer.all)
+    end
+
+    it "renders the :show template" do
+      question = create(:question)
+      get :show, id: question
+      expect(response).to render_template :show
+    end
   end
 
 end
