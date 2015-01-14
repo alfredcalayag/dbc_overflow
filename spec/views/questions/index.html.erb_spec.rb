5fd2247c58833b8336289372686060da0a58cb75
alfredcalayag <rapadura05@gmail.com>
2015-01-13 21:07:17 -0800

    Create questions index spec

--- spec/views/questions/index.html.erb_spec.rb
+++ spec/views/questions/index.html.erb_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "questions/index.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "questions/index.html.erb" do
+  let!(:question) { create :question }
+  before (:each) do
+    visit root_path
+  end
+
+  it "displays a list of all questions" do
+    expect(page).to have_content(question.title)
+  end
+
+  it "can create a new question" do
+    question = FactoryGirl.attributes_for :question
+    fill_in "title", :with => question[:title]
+    fill_in "content", :with => question[:content]
+    fill_in "user_id", :with => question[:user_id]
+    click_on "Create"
+    expect(page).to have_content question[:title]
+  end
+
 end
