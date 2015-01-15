415476b71bbf97597ae48ee9a2b99b22521c9d4d
alfredcalayag <rapadura05@gmail.com>
2015-01-14 22:05:49 -0800

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'questions#index'
-  resources :questions do
-    resources :answers
+  resources :questions, :only => [:index, :create, :show, :edit, :update] do
+    resources :answers, :only => [:create]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
