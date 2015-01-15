5b976ba8228e348c3f889d9d1cc7fff688dd223b
alfredcalayag <rapadura05@gmail.com>
2015-01-14 16:07:39 -0800

    Fix and refactor

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,77 +1,76 @@
 require 'rails_helper'
 
 describe QuestionsController do
-# Test the index and show page
 
-  describe "#index lists all questions" do
-    it "assigns @questions to Question.all" do
-      question1 = create(:question, title: "Q1")
-      question2 = create(:question, title: "Q2")
-      get :index
+  context "POST #create"
 
-      expect(assigns(:questions)).to eq Question.all
-    end
+  context "GET #index" do
+      it "assigns @questions to Question.all" do
+        question1 = create(:question, title: "Q1")
+        question2 = create(:question, title: "Q2")
+        get :index
 
-    it "renders the :index template" do
-      get :index
+        expect(assigns(:questions)).to eq Question.all
+      end
 
-      expect(response).to render_template :index
-    end
+      it "renders the :index template" do
+        get :index
+
+        expect(response).to render_template :index
+      end
+end
+
+  context "GET #show" do
+      it "assigns the requested question to @question" do
+        question = create(:question)
+        get :show, id: question
+
+        expect(assigns(:question)).to eq question
+      end
+
+      it "assigns all associated answers to @answers" do
+        question = create(:question)
+        answer1 = create(:answer, question_id: question.id)
+        answer2 = create(:answer, question_id: question.id)
+        answer3 = create(:answer, question_id: 99999999)
+        get :show, id: question
+
+        expect(assigns(:question).answers).to eq([answer1, answer2])
+        expect(assigns(:question).answers).to_not eq(Answer.all)
+      end
+
+      it "renders the :show template" do
+        question = create(:question)
+        get :show, id: question
+
+        expect(response).to render_template :show
+      end
   end
 
-
-  describe "GET #show" do
-    it "assigns the requested question to @question" do
+  context "GET #edit" do
+    it "assigns the question to @question" do
       question = create(:question)
-      get :show, id: question
+      get :edit, id: question
 
-      expect( assigns(:question) ).to eq question
+      expect(assigns(:question)).to eq question
     end
 
-    it "assigns all associated answers to @answers" do
-      question = create(:question)
-      answer1 = create(:answer, question_id: question.id)
-      answer2 = create(:answer, question_id: question.id)
-      answer3 = create(:answer, question_id: 99999999)
-      get :show, id: question
-
-      expect(question.answers).to eq([answer1, answer2])
-      expect(question.answers).to_not eq(Answer.all)
-    end
-
-    it "renders the :show template" do
-      question = create(:question)
-      get :show, id: question
-
-      expect(response).to render_template :show
-    end
-  end
-
-
-  describe "GET question#edit" do
     it "renders the :edit template" do
       question = create(:question)
       get :edit, id: question
 
       expect(response).to render_template :edit
     end
-
-    it "assigns the question to @question" do
-      question = create(:question)
-      get :edit, id: question
-
-      expect( assigns(:question) ).to eq question
-    end
   end
 
-  describe "PATCH question#update" do
+  context "PATCH #update" do
     it "changes the question's content" do
       question = create(:question)
       new_content = "banana"
       patch :update, id: question, question: {title: question.title, content: new_content}
       question.reload
 
-      expect(question.content).to eq(new_content)
+      expect(assigns(:question).content).to eq(new_content)
     end
 
     it "re-directs to the question show template if information is valid" do
